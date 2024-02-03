627f1f2737e2c9de027636bee9e5ec8ee28c6b37
Carlos Robert <crobertdmelo@gmail.com>
2024-02-03 18:47:41 -0300

    alter app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+// Dark and light mode setup
 const mode = localStorage.getItem("mode") || "";
 const toggle = document.querySelector(".toggle");
 const body = document.querySelector("body");
@@ -8,3 +9,8 @@ toggle.addEventListener("click", () => {
     localStorage.setItem("mode", mode === "light" ? "" : "light");
     body.classList.toggle("light");
 });
+
+// Dynamic year on footer 
+const yearSpan = document.getElementById('year');
+const year = new Date().getFullYear();
+yearSpan.innerText = year;
